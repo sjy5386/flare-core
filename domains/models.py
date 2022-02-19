eb16ade25dd3ace10eab84e15dd2a6c255c02d60
Seokjin Yoon <sjy5386@gmail.com>
2022-02-19 11:32:21 +0900

    Add Domain model

--- domains/models.py
+++ domains/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Domain(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
