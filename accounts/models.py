add144b8caf1b987b8391e817b9d31393b6c7242
Seokjin Yoon <sjy5386@gmail.com>
2022-02-19 11:34:53 +0900

    Add User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
