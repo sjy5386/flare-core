4efb229dcd840af991c516473944893720164a88
Seokjin Yoon <sjy5386@gmail.com>
2022-02-22 13:07:29 +0900

    Add SubdomainForm

--- subdomains/forms.py
+++ subdomains/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 
-from .models import Contact
+from .models import Contact, Subdomain
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         exclude = ['created_at', 'updated_at', 'user']
+
+
+class SubdomainForm(forms.ModelForm):
+    class Meta:
+        model = Subdomain
+        exclude = ['created_at', 'updated_at', 'user', 'expiry', 'status']
 
 
 class RecordForm(forms.Form):
