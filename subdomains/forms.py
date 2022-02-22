467971b8beaac19b3d9c31e1fced1e862ea89e88
Seokjin Yoon <sjy5386@gmail.com>
2022-02-22 12:42:37 +0900

    Add ContactForm

--- subdomains/forms.py
+++ subdomains/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        exclude = ['created_at', 'updated_at', 'user']
+
 
 class RecordForm(forms.Form):
     name = forms.CharField()
