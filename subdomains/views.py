b0d87f6ae61b3eeb7644856d09f891a89cf6525f
Seokjin Yoon <sjy5386@gmail.com>
2022-02-25 23:07:28 +0900

    Add SubdomainListView

--- subdomains/views.py
+++ subdomains/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Subdomain
+
+
+@method_decorator(login_required, name='dispatch')
+class SubdomainListView(ListView):
+    template_name = 'subdomains/list.html'
+
+    def get_queryset(self):
+        return Subdomain.objects.filter(user=self.request.user)
