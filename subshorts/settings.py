5b56b22d816264960245036d17eb16c8fa4bf5e2
Seokjin Yoon <sjy5386@gmail.com>
2022-02-26 18:28:11 +0900

    Apply django-bootstrap5

--- subshorts/settings.py
+++ subshorts/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_bootstrap5',
+
     'accounts.apps.AccountsConfig',
     'contacts.apps.ContactsConfig',
     'domains.apps.DomainsConfig',
