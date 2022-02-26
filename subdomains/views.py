ecb1dc0918ed096276c8182260135e12d334edac
Seokjin Yoon <sjy5386@gmail.com>
2022-02-26 20:11:51 +0900

    Add SubdomainCreateView

--- subdomains/views.py
+++ subdomains/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
+from .forms import SubdomainForm
 from .models import Subdomain
 
 
@@ -11,3 +13,16 @@ class SubdomainListView(ListView):
 
     def get_queryset(self):
         return Subdomain.objects.filter(user=self.request.user)
+
+
+@method_decorator(login_required, name='dispatch')
+class SubdomainCreateView(CreateView):
+    template_name = 'subdomains/create.html'
+    form_class = SubdomainForm
+    success_url = reverse_lazy('subdomain_list')
+
+    def form_valid(self, form):
+        subdomain = form.save(commit=False)
+        subdomain.user = self.request.user
+        subdomain.save()
+        return super(SubdomainCreateView, self).form_valid(form)
