2bedd783df51426fd9ad1a9b3b5ff48e2207484f
Seokjin Yoon <sjy5386@gmail.com>
2022-02-26 17:54:25 +0900

    Add ContactDetailView

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.ContactListView.as_view(), name='contact_list'),
     path('create/', views.ContactCreateView.as_view(), name='contact_create'),
+    path('<int:id>/', views.ContactDetailView.as_view(), name='contact_detail'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from .forms import ContactForm
 from .models import Contact
@@ -26,3 +27,11 @@ class ContactCreateView(CreateView):
         contact.user = self.request.user
         contact.save()
         return super(ContactCreateView, self).form_valid(form)
+
+
+@method_decorator(login_required, name='dispatch')
+class ContactDetailView(DetailView):
+    template_name = 'contacts/detail.html'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Contact, id=self.kwargs['id'], user=self.request.user)
