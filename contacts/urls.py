f203c297cfe1f5d8cb5efd2134568a4a45990e10
Seokjin Yoon <sjy5386@gmail.com>
2022-02-26 18:01:01 +0900

    Add ContactUpdateView

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.ContactListView.as_view(), name='contact_list'),
     path('create/', views.ContactCreateView.as_view(), name='contact_create'),
     path('<int:id>/', views.ContactDetailView.as_view(), name='contact_detail'),
+    path('<int:id>/update/', views.ContactUpdateView.as_view(), name='contact_update'),
 ]
