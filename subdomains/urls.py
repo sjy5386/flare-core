9e89ba8060900c23c7164d87cecf39429591bff5
Seokjin Yoon <sjy5386@gmail.com>
2022-02-27 15:26:54 +0900

    Add SubdomainDeleteView

--- subdomains/urls.py
+++ subdomains/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', views.SubdomainCreateView.as_view(), name='subdomain_create'),
     path('<int:id>/', views.SubdomainDetailView.as_view(), name='subdomain_detail'),
     path('<int:id>/update/', views.SubdomainUpdateView.as_view(), name='subdomain_update'),
+    path('<int:id>/delete/', views.SubdomainDeleteView.as_view(), name='subdomain_delete'),
 ]
--- subdomains/views.py
+++ subdomains/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from .forms import SubdomainForm
 from .models import Subdomain
@@ -41,6 +41,15 @@ class SubdomainDetailView(DetailView):
 class SubdomainUpdateView(UpdateView):
     template_name = 'subdomains/update.html'
     form_class = SubdomainForm
+    success_url = reverse_lazy('subdomain_list')
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Subdomain, id=self.kwargs['id'], user=self.request.user)
+
+
+@method_decorator(login_required, name='dispatch')
+class SubdomainDeleteView(DeleteView):
+    template_name = 'subdomains/delete.html'
     success_url = reverse_lazy('subdomain_list')
 
     def get_object(self, queryset=None):
