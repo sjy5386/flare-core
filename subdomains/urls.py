2d92c4c173ce9c446f7f778e3a8a9d02b71420d6
Seokjin Yoon <sjy5386@gmail.com>
2022-02-27 17:52:42 +0900

    Add subdomains search view

--- subdomains/urls.py
+++ subdomains/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.SubdomainListView.as_view(), name='subdomain_list'),
+    path('search/', views.search, name='subdomain_search'),
     path('create/', views.SubdomainCreateView.as_view(), name='subdomain_create'),
     path('<int:id>/', views.SubdomainDetailView.as_view(), name='subdomain_detail'),
     path('<int:id>/update/', views.SubdomainUpdateView.as_view(), name='subdomain_update'),
--- subdomains/views.py
+++ subdomains/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_GET
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from .forms import SubdomainForm
+from domains.models import Domain
+from .forms import SubdomainForm, SubdomainSearchForm
 from .models import Subdomain
 
 
@@ -14,6 +16,27 @@ class SubdomainListView(ListView):
 
     def get_queryset(self):
         return Subdomain.objects.filter(user=self.request.user)
+
+
+@require_GET
+def search(request):
+    q = request.GET.get('q', '')
+    domain = request.GET.getlist('domain', list(map(lambda e: e.id, Domain.objects.filter(is_active=True))))
+    hide_unavailable = (lambda x: x == 'on')(request.GET.get('hide_unavailable', 'off'))
+    results = {}
+    for domain_id in domain:
+        subdomain = q + '.' + Domain.objects.get(id=domain_id).name
+        availability = len(Subdomain.objects.filter(name=q, domain_id=domain_id)) == 0
+        if availability or not hide_unavailable:
+            results[subdomain] = availability
+    return render(request, 'subdomains/search.html', {
+        'form': SubdomainSearchForm(initial={
+            'q': q,
+            'domain': domain,
+            'hide_unavailable': hide_unavailable
+        }),
+        'results': results.items()
+    })
 
 
 @method_decorator(login_required, name='dispatch')
