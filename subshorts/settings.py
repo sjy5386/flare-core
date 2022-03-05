dfa465b173f87d49ab1ffad424011104635f61d4
Seokjin Yoon <sjy5386@gmail.com>
2022-03-05 22:13:00 +0900

    Add boards app

--- subshorts/settings.py
+++ subshorts/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',
 
     'accounts.apps.AccountsConfig',
+    'boards.apps.BoardsConfig',
     'contacts.apps.ContactsConfig',
     'domains.apps.DomainsConfig',
     'shorturls.apps.ShorturlsConfig',
