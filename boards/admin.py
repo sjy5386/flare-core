d7d6f569c90ed3f9ae31b3f448bcbc55ef522b47
Seokjin Yoon <sjy5386@gmail.com>
2022-03-05 22:51:21 +0900

    Add Board model

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from boards.models import *
+
+admin.site.register(Board)
--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=63)
+    title = models.CharField(max_length=255)
