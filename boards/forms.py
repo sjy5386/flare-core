1a6d93fee91db6e9474e3648b8ec5c8cb8eb9a5a
Seokjin Yoon <sjy5386@gmail.com>
2022-03-06 00:08:33 +0900

    Add CommentForm

--- boards/forms.py
+++ boards/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['created_at', 'updated_at', 'board', 'user', 'views']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['created_at', 'updated_at', 'post', 'user']
