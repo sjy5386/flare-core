29bbe571b53246eed8ae7d1cb1469d33480b74cd
Seokjin Yoon <sjy5386@gmail.com>
2022-03-05 23:30:25 +0900

    Add Post model

--- boards/admin.py
+++ boards/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from boards.models import *
 
 admin.site.register(Board)
+admin.site.register(Post)
--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from subshorts.settings import AUTH_USER_MODEL
+
 
 class Board(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -7,3 +9,17 @@ class Board(models.Model):
 
     name = models.CharField(max_length=63)
     title = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+
+    title = models.CharField(max_length=255)
+    is_private = models.BooleanField(default=False)
+    content = models.TextField()
+
+    views = models.PositiveIntegerField(default=0)
