b793601508f70496f087ccfd81a389a7240026d2
Seokjin Yoon <sjy5386@gmail.com>
2022-03-05 23:45:08 +0900

    Add PostCreateView

--- boards/urls.py
+++ boards/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<str:board_name>/', views.PostListView.as_view(), name='post_list'),
+    path('<str:board_name>/create/', views.PostCreateView.as_view(), name='post_create'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,26 @@
-from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView
 
-from .models import Post
+from .forms import PostForm
+from .models import Board, Post
 
 
 class PostListView(ListView):
     def get_queryset(self):
         return Post.objects.filter(board__name=self.kwargs['board_name'])
+
+
+@method_decorator(login_required, name='dispatch')
+class PostCreateView(CreateView):
+    form_class = PostForm
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.board = get_object_or_404(Board, name=self.kwargs['board_name'])
+        post.user = self.request.user
+        post.save()
+        return super(PostCreateView, self).form_valid(form)
