00eda69dd0c8b1be4a0878844d9224e3cdcc8811
Seokjin Yoon <sjy5386@gmail.com>
2022-03-07 22:10:56 +0900

    Update PostDetailView

--- boards/views.py
+++ boards/views.py
@@ -3,10 +3,10 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, FormView
 
-from .forms import PostForm
-from .models import Board, Post
+from .forms import PostForm, CommentForm
+from .models import Board, Post, Comment
 
 
 class BoardListView(ListView):
@@ -39,7 +39,9 @@ class PostCreateView(CreateView):
         return reverse('post_list', kwargs=self.kwargs)
 
 
-class PostDetailView(DetailView):
+class PostDetailView(DetailView, FormView):
+    form_class = CommentForm
+
     def get_object(self, queryset=None):
         post = get_object_or_404(Post, id=self.kwargs['id'], board__name=self.kwargs['board_name'])
         user = self.request.user
@@ -52,7 +54,18 @@ class PostDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(PostDetailView, self).get_context_data(**kwargs)
         context['board'] = get_object_or_404(Board, name=self.kwargs['board_name'])
+        context['comments'] = Comment.objects.filter(post_id=self.kwargs['id'])
         return context
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, id=self.kwargs['id'])
+        comment.user = self.request.user
+        comment.save()
+        return super(PostDetailView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs=self.kwargs)
 
 
 @method_decorator(login_required, name='dispatch')
@@ -65,7 +78,7 @@ class PostUpdateView(UpdateView):
                                  user=self.request.user)
 
     def get_success_url(self):
-        return reverse('post_list', kwargs={'board_name': self.kwargs['board_name']})
+        return reverse('post_detail', kwargs=self.kwargs)
 
 
 @method_decorator(login_required, name='dispatch')
