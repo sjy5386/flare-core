4d0ffa35915b4b8796f05dc8084dceb66b051a8a
Seokjin Yoon <sjy5386@gmail.com>
2022-03-07 14:46:15 +0900

    Update BaseProvider

--- subdomains/providers.py
+++ subdomains/providers.py
@@ -19,3 +19,19 @@ class BaseProvider:
 
     def delete_record(self, subdomain: Subdomain, identifier):
         pass
+
+    def export_zone(self, subdomain: Subdomain) -> str:
+        records = self.list_records(subdomain)
+        zone = ''
+        for record in records:
+            zone += str(record) + '\n'
+        return zone
+
+    def import_zone(self, subdomain: Subdomain, zone: str):
+        lines = zone.splitlines()
+        for line in lines:
+            if line[0] == ';':
+                continue
+            r = line.split()
+            record = Record(name=r[0], ttl=int(r[1]), record_type=r[3], data=' '.join(r[4:]))
+            self.create_record(subdomain, record)
