17b0803ffef3c5040bad6b33f0ba663e5995b151
Seokjin Yoon <sjy5386@gmail.com>
2022-03-08 16:58:06 +0900

    Update RecordForm

--- records/forms.py
+++ records/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class RecordForm(forms.Form):
     name = forms.CharField()
-    ttl = forms.IntegerField(min_value=0)
-    r_type = forms.CharField()
+    ttl = forms.IntegerField(min_value=0, initial=3600)
+    r_type = forms.CharField(initial='A')
     data = forms.CharField()
