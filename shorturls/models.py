975312d9c525ada9f9314923699e57fbb39ae442
Seokjin Yoon <sjy5386@gmail.com>
2022-03-08 20:55:51 +0900

    Add ShortUrl model

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from base.settings import AUTH_USER_MODEL
+from domains.models import Domain
+
+
+class ShortUrl(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    domain = models.ForeignKey(Domain, on_delete=models.RESTRICT)
+
+    name = models.CharField(max_length=255)
+
+    short = models.CharField(max_length=31)
+    long_url = models.URLField(max_length=2047)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['domain', 'short'], name='unique_domain_short')
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def get_short_url(self):
+        return f'https://{self.domain.name}/{self.short}'
