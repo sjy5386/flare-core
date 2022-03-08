f4e6264094c8de868688503f245dc30319e67e21
Seokjin Yoon <sjy5386@gmail.com>
2022-03-08 22:21:45 +0900

    Add FirebaseDynamicLinksProvider

--- shorturls/providers.py
+++ shorturls/providers.py
@@ -1,7 +1,31 @@
+import os
+
+import requests
+
 from domains.models import Domain
-from .models import ShortUrl
 
 
 class BaseProvider:
-    def create_short_url(self, domain: Domain, name: str, long_url: str) -> ShortUrl:
+    def create_short_url(self, domain: Domain, name: str, long_url: str) -> str:
         pass
+
+
+class FirebaseDynamicLinksProvider(BaseProvider):
+    api_key = os.environ.get("FIREBASE_WEB_API_KEY")
+
+    def create_short_url(self, domain: Domain, name: str, long_url: str) -> str:
+        request_body = {
+            'dynamicLinkInfo': {
+                'domainUriPrefix': f'https://{domain.name}',
+                'link': long_url
+            },
+            'suffix': {
+                'option': 'SHORT'
+            }
+        }
+        response = requests.post('https://firebasedynamiclinks.googleapis.com/v1/shortLinks',
+                                 params={'key': self.api_key}, json=request_body)
+        return response.json()['shortLink'].split('/')[-1]
+
+
+PROVIDER_CLASS = FirebaseDynamicLinksProvider
