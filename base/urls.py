bd2192eb24addd7b825cee3db787d90115641cab
Seokjin Yoon <sjy5386@gmail.com>
2022-03-09 23:44:39 +0900

    Add index view

--- base/urls.py
+++ base/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('boards/', include('boards.urls')),
