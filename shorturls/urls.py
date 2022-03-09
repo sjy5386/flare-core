1f24a2fd4d8433aabc77be3002c863619eb9eecf
Seokjin Yoon <sjy5386@gmail.com>
2022-03-09 12:21:10 +0900

    Add create_short_url view

--- shorturls/urls.py
+++ shorturls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_short_urls, name='shorturl_list'),
+    path('create/', views.create_short_url, name='shorturl_create'),
 ]
--- shorturls/views.py
+++ shorturls/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views.decorators.http import require_GET
 
+from domains.models import Domain
+from .forms import ShortUrlForm
 from .models import ShortUrl
+from .providers import PROVIDER_CLASS
 
 
 @login_required
@@ -11,3 +15,19 @@ def list_short_urls(request):
     return render(request, 'shorturls/list.html', {
         'object_list': ShortUrl.objects.filter(user=request.user)
     })
+
+
+@login_required
+def create_short_url(request):
+    if request.method == 'GET':
+        return render(request, 'shorturls/create.html', {
+            'form': ShortUrlForm()
+        })
+    elif request.method == 'POST':
+        provider = PROVIDER_CLASS()
+        domain = get_object_or_404(Domain, id=request.POST['domain'])
+        name = request.POST['name']
+        long_url = request.POST['long_url']
+        short = provider.create_short_url(domain, name, long_url)
+        ShortUrl(domain=domain, name=name, short=short, long_url=long_url).save()
+        return redirect(reverse('shorturl_list'))
