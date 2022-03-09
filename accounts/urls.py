be619ce1ebc43722a35b48849517e17a68f9208f
Seokjin Yoon <sjy5386@gmail.com>
2022-03-09 22:31:10 +0900

    Add register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .decorators import logout_required
+
+
+@logout_required
+def register(request: HttpRequest):
+    if request.method == 'GET':
+        return render(request, 'accounts/register.html', {
+            'form': UserCreationForm()
+        })
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 != password2:
+            return HttpResponse('Your password does not match.')
+        user = get_user_model().objects.create_user(username=username, password=password1)
+        login(request, user)
+        return HttpResponse('success')
