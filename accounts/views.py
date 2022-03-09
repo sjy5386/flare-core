f94cedb331ef00817b91acc3a614493fe57e15b0
Seokjin Yoon <sjy5386@gmail.com>
2022-03-09 23:09:34 +0900

    Add UserRegisterForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.decorators.http import require_GET
 
 from .decorators import logout_required
+from .forms import UserRegisterForm
 
 
 @login_required
@@ -19,7 +19,7 @@ def profile(request: HttpRequest):
 def register(request: HttpRequest):
     if request.method == 'GET':
         return render(request, 'accounts/register.html', {
-            'form': UserCreationForm()
+            'form': UserRegisterForm()
         })
     elif request.method == 'POST':
         username = request.POST['username']
@@ -27,6 +27,7 @@ def register(request: HttpRequest):
         password2 = request.POST['password2']
         if password1 != password2:
             return HttpResponse('Your password does not match.')
-        user = get_user_model().objects.create_user(username=username, password=password1)
+        email = request.POST['email']
+        user = get_user_model().objects.create_user(username=username, password=password1, email=email)
         login(request, user)
         return redirect(reverse('profile'))
