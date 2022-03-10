11620d8694e127267a3beea96506e3263fc1d1a2
Seokjin Yoon <sjy5386@gmail.com>
2022-03-10 17:02:33 +0900

    Add ZoneImportForm

--- records/forms.py
+++ records/forms.py
@@ -6,3 +6,7 @@ class RecordForm(forms.Form):
     ttl = forms.IntegerField(min_value=0, initial=3600)
     r_type = forms.CharField(initial='A')
     data = forms.CharField()
+
+
+class ZoneImportForm(forms.Form):
+    zone = forms.CharField(widget=forms.Textarea)
