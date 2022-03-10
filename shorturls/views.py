9fc9bb95d82636e77c7e823623dd513c7a6af9f4
Seokjin Yoon <sjy5386@gmail.com>
2022-03-10 18:49:30 +0900

    Update create_short_url view

--- shorturls/views.py
+++ shorturls/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
@@ -5,7 +7,7 @@ from django.views.decorators.http import require_GET
 
 from domains.models import Domain
 from .forms import ShortUrlForm
-from .models import ShortUrl
+from .models import ShortUrl, BlockedDomain
 from .providers import PROVIDER_CLASS
 
 
@@ -28,6 +30,7 @@ def create_short_url(request):
         domain = get_object_or_404(Domain, id=request.POST['domain'])
         name = request.POST['name']
         long_url = request.POST['long_url']
-        short = provider.create_short_url(domain, name, long_url)
-        ShortUrl(domain=domain, name=name, short=short, long_url=long_url).save()
+        if len(BlockedDomain.objects.filter(domain=urlparse(long_url).netloc)) == 0:
+            short = provider.create_short_url(domain, long_url)
+            ShortUrl(domain=domain, name=name, short=short, long_url=long_url).save()
         return redirect(reverse('shorturl_list'))
