3cabcc7a9f6149e2752f3e63d0ddf3689d41c027
Seokjin Yoon <sjy5386@gmail.com>
2022-03-11 23:47:12 +0900

    Update PostForm

--- boards/forms.py
+++ boards/forms.py
@@ -6,7 +6,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude = ['created_at', 'updated_at', 'board', 'user', 'views']
+        fields = ['title', 'is_private', 'content']
 
 
 class CommentForm(forms.ModelForm):
