14ca668900d0c254e8b21cc075a5c3f0ef199315
Seokjin Yoon <sjy5386@gmail.com>
2022-03-12 17:22:20 +0900

    Update RecordForm

--- records/forms.py
+++ records/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 
+from .types import Record
+
 
 class RecordForm(forms.Form):
     name = forms.CharField()
-    ttl = forms.IntegerField(min_value=0, initial=3600)
-    r_type = forms.CharField(initial='A')
+    ttl = forms.IntegerField(label='TTL', min_value=0, initial=3600)
+    r_type = forms.ChoiceField(label='Type', choices=sorted(map(lambda e: (e, e), Record.get_available_types())),
+                               initial='A')
     data = forms.CharField()
 
 
