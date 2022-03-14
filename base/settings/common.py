a7aa2209e17b3df1be62110ead2b776c6b62c660
Seokjin Yoon <sjy5386@gmail.com>
2022-03-14 15:24:09 +0900

    Update database engine

--- base/settings/common.py
+++ base/settings/common.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,8 +83,17 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'subshorts',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
     }
 }
 
--- base/settings/prod.py
+++ base/settings/prod.py
@@ -1,3 +1,5 @@
 from .common import *
 
 DEBUG = False
+
+DATABASES['default']['HOST'] = '172.17.0.1'
