ba213226f2509dbb423bb3cfdc1c26385af6a403
Seokjin Yoon <sjy5386@gmail.com>
2022-03-16 00:32:55 +0900

    Update settings/prod.py

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -1,5 +1,9 @@
+from django.core.management.utils import get_random_secret_key
+
 from .common import *
 
 DEBUG = False
+
+SECRET_KEY = get_random_secret_key()
 
 DATABASES['default']['HOST'] = 'host.docker.internal'
