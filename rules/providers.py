9d59b30fc164dd82f3618778f2f7a0914fed50b4
Seokjin Yoon <sjy5386@gmail.com>
2022-03-20 20:20:11 +0900

    Add rules BaseProvider

---
+++ rules/providers.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from subdomains.models import Subdomain
+from .types import PageRule
+
+
+class BaseProvider:
+    def list_rules(self, subdomain: Subdomain) -> List[PageRule]:
+        pass
+
+    def create_rule(self, subdomain: Subdomain, rule: PageRule) -> PageRule:
+        pass
+
+    def retrieve_rule(self, subdomain: Subdomain, identifier) -> PageRule:
+        pass
+
+    def update_rule(self, subdomain: Subdomain, identifier, rule: PageRule) -> PageRule:
+        pass
+
+    def delete_rule(self, subdomain: Subdomain, identifier):
+        pass
