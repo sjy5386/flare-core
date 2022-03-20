270e7b853eb2aae74cc4986677d3a5be2b0244b2
Seokjin Yoon <sjy5386@gmail.com>
2022-03-20 20:25:42 +0900

    Add rules MockProvider

--- rules/providers.py
+++ rules/providers.py
@@ -19,3 +19,38 @@ class BaseProvider:
 
     def delete_rule(self, subdomain: Subdomain, identifier):
         pass
+
+
+class MockProvider(BaseProvider):
+    i = 1
+    rules = []
+
+    def list_rules(self, subdomain: Subdomain) -> List[PageRule]:
+        return self.rules
+
+    def create_rule(self, subdomain: Subdomain, rule: PageRule) -> PageRule:
+        rule.identifier = self.i
+        self.i += 1
+        self.rules.append(rule)
+        return rule
+
+    def retrieve_rule(self, subdomain: Subdomain, identifier) -> PageRule:
+        for r in self.rules:
+            if r.identifier == identifier:
+                return r
+
+    def update_rule(self, subdomain: Subdomain, identifier, rule: PageRule) -> PageRule:
+        for r in self.rules:
+            if r.identifier == identifier:
+                r = rule
+                r.identifier = identifier
+                return r
+
+    def delete_rule(self, subdomain: Subdomain, identifier):
+        for r in self.rules:
+            if r.identifier == identifier:
+                self.rules.remove(r)
+                return
+
+
+PROVIDER_CLASS = MockProvider
