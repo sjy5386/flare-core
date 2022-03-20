ffab11e2ea8f2cc4fff0ca5cc3a54a85f0521449
Seokjin Yoon <sjy5386@gmail.com>
2022-03-20 20:14:24 +0900

    Add PageRule class

---
+++ rules/types.py
@@ -0,0 +1,13 @@
+class PageRule:
+    identifier = None
+
+    domain: str
+    owner: str
+
+    forwarding_protocol: str = None
+    forwarding_domain: str = None
+    forwarding_code: int = None
+    forwarding_path: bool = None
+
+    parking_title: str = None
+    parking_content: str = None
