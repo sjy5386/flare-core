fd544aea9a64748f670683ec6ebe4d64c9022d22
Seokjin Yoon <sjy5386@gmail.com>
2022-03-30 20:48:27 +0900

    Add apis/urls.py

--- base/urls.py
+++ base/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('apis.urls')),
     path('boards/', include('boards.urls')),
     path('contacts/', include('contacts.urls')),
     path('shorturls/', include('shorturls.urls')),
