0a07301a6e686b0dbe56cec3204334ef75f92ae3
Seokjin Yoon <sjy5386@gmail.com>
2022-03-31 14:03:49 +0900

    Update subdomain api

--- apis/serializers.py
+++ apis/serializers.py
@@ -19,6 +19,7 @@ class ContactSerializer(serializers.ModelSerializer):
 class SubdomainSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     expiry = serializers.DateTimeField(default=datetime.datetime.now() + datetime.timedelta(days=90), read_only=True)
+    full_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Subdomain
@@ -30,3 +31,18 @@ class SubdomainSerializer(serializers.ModelSerializer):
                 fields=['name', 'domain'],
             ),
         ]
+
+    def get_fields(self):
+        fields = super(SubdomainSerializer, self).get_fields()
+        request = self.context.get('request')
+        contacts = Contact.objects.filter(user=request.user)
+        fields.update({
+            'registrant': serializers.PrimaryKeyRelatedField(queryset=contacts),
+            'admin': serializers.PrimaryKeyRelatedField(queryset=contacts),
+            'tech': serializers.PrimaryKeyRelatedField(queryset=contacts),
+            'billing': serializers.PrimaryKeyRelatedField(queryset=contacts),
+        })
+        return fields
+
+    def get_full_name(self, obj):
+        return str(obj)
