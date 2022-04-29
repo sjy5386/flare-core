e9cfbde8dc25056a523c4a5c680d6e43f6fa0f36
Seokjin Yoon <sjy5386@gmail.com>
2022-04-29 10:18:43 +0900

    Rename contact templates

--- contacts/views.py
+++ contacts/views.py
@@ -10,15 +10,13 @@ from .models import Contact
 
 @method_decorator(login_required, name='dispatch')
 class ContactListView(ListView):
-    template_name = 'contacts/list.html'
-
     def get_queryset(self):
         return Contact.objects.filter(user=self.request.user)
 
 
 @method_decorator(login_required, name='dispatch')
 class ContactCreateView(CreateView):
-    template_name = 'contacts/create.html'
+    template_name = 'contacts/contact_create.html'
     form_class = ContactForm
     success_url = reverse_lazy('contact_list')
 
@@ -31,15 +29,12 @@ class ContactCreateView(CreateView):
 
 @method_decorator(login_required, name='dispatch')
 class ContactDetailView(DetailView):
-    template_name = 'contacts/detail.html'
-
     def get_object(self, queryset=None):
         return get_object_or_404(Contact, id=self.kwargs['id'], user=self.request.user)
 
 
 @method_decorator(login_required, name='dispatch')
 class ContactUpdateView(UpdateView):
-    template_name = 'contacts/update.html'
     form_class = ContactForm
     success_url = reverse_lazy('contact_list')
 
@@ -49,7 +44,6 @@ class ContactUpdateView(UpdateView):
 
 @method_decorator(login_required, name='dispatch')
 class ContactDeleteView(DeleteView):
-    template_name = 'contacts/delete.html'
     success_url = reverse_lazy('contact_list')
 
     def get_object(self, queryset=None):
