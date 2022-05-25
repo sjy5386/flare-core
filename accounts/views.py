fafe4c8ca045e66f7eebafa0b8dcd6c7688dd7c4
Seokjin Yoon <sjy5386@gmail.com>
2022-05-25 13:59:35 +0900

    Update register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.decorators.http import require_GET
@@ -25,12 +26,14 @@ def register(request: HttpRequest):
     elif request.method == 'POST':
         captcha = CaptchaForm(request.POST).is_valid()
         if not captcha:
-            return HttpResponse(captcha)
+            messages.add_message(request, messages.ERROR, captcha)
+            return redirect('register')
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         if password1 != password2:
-            return HttpResponse('Your password does not match.')
+            messages.add_message(request, messages.ERROR, 'Your password does not match.')
+            return redirect('register')
         email = request.POST['email']
         user = get_user_model().objects.create_user(username=username, password=password1, email=email)
         login(request, user)
