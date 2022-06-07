a9e2f9e8a233e17d7121449e82cda1ed6cd14f41
Seokjin Yoon <sjy5386@gmail.com>
2022-06-07 20:43:41 +0900

    Update Domain model

--- domains/models.py
+++ domains/models.py
@@ -8,5 +8,8 @@ class Domain(models.Model):
     name = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
 
+    records_provider_id = models.CharField(max_length=255, null=True)
+    shorturls_provider_id = models.CharField(max_length=255, null=True)
+
     def __str__(self):
         return self.name
