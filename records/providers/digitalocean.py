c1f5128fc3942bb19be571681a23053d5e0c544c
Seokjin Yoon <sjy5386@gmail.com>
2022-08-11 20:51:16 +0900

    Update DigitalOceanRecordProvider

--- records/providers/digitalocean.py
+++ records/providers/digitalocean.py
@@ -1,83 +1,82 @@
 import os
-from typing import List
+from typing import Any, Dict, List, Optional
 
 import digitalocean
 
-from subdomains.models import Subdomain
+from domains.models import Domain
 from .base import BaseRecordProvider
-from ..types import Record
 
 
 class DigitalOceanRecordProvider(BaseRecordProvider):
     token = os.environ.get('DIGITALOCEAN_ACCESS_TOKEN')
 
-    def list_records(self, subdomain: Subdomain) -> List[Record]:
-        domain = digitalocean.Domain(token=self.token, name=subdomain.domain.name)
-        records = domain.get_records()
-        return list(map(self.provider_record_object_to_record_object,
-                        filter(lambda e: e.name.endswith(subdomain.name), records)))
+    def list_records(self, subdomain_name: str, domain: Domain) -> List[Dict[str, Any]]:
+        do_domain = digitalocean.Domain(token=self.token, name=domain.name)
+        records = do_domain.get_records()
+        return list(map(self.record_to_dict, filter(lambda e: e.name.endswith(subdomain_name), records)))
 
-    def create_record(self, subdomain: Subdomain, record: Record) -> Record:
-        if not record.name.endswith(subdomain.name):
-            return record
-        domain = digitalocean.Domain(token=self.token, name=subdomain.domain.name)
-        new_record = domain.create_new_domain_record(
-            name=record.name,
-            ttl=record.ttl,
-            type=record.type,
-            data=record.target,
-            priority=record.priority,
-            weight=record.priority,
-            port=record.port,
+    def create_record(self, subdomain_name: str, domain: Domain, **kwargs) -> Dict[str, Any]:
+        if not kwargs.get('name', subdomain_name).endswith(subdomain_name):
+            return kwargs
+        do_domain = digitalocean.Domain(token=self.token, name=domain.name)
+        new_record = do_domain.create_new_domain_record(
+            name=kwargs.get('name'),
+            ttl=kwargs.get('ttl'),
+            type=kwargs.get('ttl'),
+            data=kwargs.get('target'),
+            priority=kwargs.get('priority'),
+            weight=kwargs.get('weight'),
+            port=kwargs.get('port'),
         )
-        record.id = new_record['domain_record']['id']
-        return record
+        kwargs['provider_id'] = new_record['domain_record']['id']
+        return kwargs
 
-    def retrieve_record(self, subdomain: Subdomain, id) -> Record:
-        domain = digitalocean.Domain(token=self.token, name=subdomain.domain.name)
-        id = int(id)
-        records = domain.get_records()
+    def retrieve_record(self, subdomain_name: str, domain: Domain, provider_id: str) -> Optional[Dict[str, Any]]:
+        do_domain = digitalocean.Domain(token=self.token, name=domain.name)
+        do_id = int(provider_id)
+        records = do_domain.get_records()
         for r in records:
-            if r.id == id and r.name.endswith(subdomain.name):
-                return self.provider_record_object_to_record_object(r)
+            if r.id == do_id and r.name.endswith(subdomain_name):
+                return self.record_to_dict(r)
 
-    def update_record(self, subdomain: Subdomain, id, record: Record) -> Record:
-        if not record.name.endswith(subdomain.name):
-            return record
-        domain = digitalocean.Domain(token=self.token, name=subdomain.domain.name)
-        id = int(id)
-        records = domain.get_records()
+    def update_record(self, subdomain_name: str, domain: Domain, provider_id: str, **kwargs
+                      ) -> Optional[Dict[str, Any]]:
+        if not kwargs.get('name', subdomain_name).endswith(subdomain_name):
+            return kwargs
+        do_domain = digitalocean.Domain(token=self.token, name=subdomain_name)
+        do_id = int(provider_id)
+        records = do_domain.get_records()
         for r in records:
-            if r.id == id:
-                r.name = record.name
-                r.ttl = record.ttl
-                r.type = record.type
-                r.data = record.target
-                if record.type == 'MX' or record.type == 'SRV':
-                    r.priority = record.priority
-                if record.type == 'SRV':
-                    r.weight = record.weight
-                    r.port = record.port
+            if r.id == do_id:
+                r.ttl = kwargs.get('ttl')
+                r.data = kwargs.get('target')
+                if kwargs.get('type') in ['MX', 'SRV']:
+                    r.priority = kwargs.get('priority')
+                if kwargs.get('type') in ['SRV']:
+                    r.weight = kwargs.get('weight')
+                    r.port = kwargs.get('port')
                 r.save()
-        return record
+        return kwargs
 
-    def delete_record(self, subdomain: Subdomain, id):
-        domain = digitalocean.Domain(token=self.token, name=subdomain.domain.name)
-        id = int(id)
-        records = domain.get_records()
+    def delete_record(self, subdomain_name: str, domain: Domain, provider_id: str) -> None:
+        do_domain = digitalocean.Domain(token=self.token, name=subdomain_name)
+        do_id = int(provider_id)
+        records = do_domain.get_records()
         for r in records:
-            if r.id == id and r.name.endswith(subdomain.name):
+            if r.id == do_id and r.name.endswith(subdomain_name):
                 r.destroy()
 
-    def provider_record_object_to_record_object(self, provider_record_object) -> Record:
-        kwargs = {
-            'identifier': provider_record_object.id
+    @staticmethod
+    def record_to_dict(record) -> Dict[str, Any]:
+        d = {
+            'provider_id': record.id,
+            'name': record.name,
+            'ttl': record.ttl,
+            'type': record.type,
+            'target': record.data,
         }
-        if provider_record_object.type == 'MX' or provider_record_object == 'SRV':
-            kwargs['priority'] = provider_record_object.priority
-        if provider_record_object.type == 'SRV':
-            kwargs['weight'] = provider_record_object.weight
-            kwargs['port'] = provider_record_object.port
-        record = Record(provider_record_object.name, provider_record_object.ttl, provider_record_object.type,
-                        provider_record_object.data, **kwargs)
-        return record
+        if record.type in ['MX', 'SRV']:
+            d.update({'priority': record.priority})
+        if record.type in ['SRV']:
+            d.update({'weight': record.weight, 'port': record.port})
+        return d
