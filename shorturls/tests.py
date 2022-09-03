542d6438f0473f9668726b385592118d866f9250
Seokjin Yoon <sjy5386@gmail.com>
2022-09-03 22:11:27 +0900

    Add ShortUrlTest

--- shorturls/tests.py
+++ shorturls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import ShortUrl
+
+
+class ShortUrlTest(TestCase):
+    def test_split_short_url(self):
+        result = ShortUrl.split_short_url('https://example.com/index')
+        self.assertEqual(result, ('example.com', 'index'))
+
+    def test_join_short_url(self):
+        result = ShortUrl.join_short_url('example.com', 'index')
+        self.assertEqual(result, 'https://example.com/index')
