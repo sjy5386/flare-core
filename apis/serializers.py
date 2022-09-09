a66f5bacb59a8d017ea15923336d04e40087db1d
Seokjin Yoon <sjy5386@gmail.com>
2022-09-09 14:41:06 +0900

    Remove BaseRecordSerializer and RecordSerializer

--- apis/serializers.py
+++ apis/serializers.py
@@ -5,7 +5,6 @@ from rest_framework.validators import UniqueTogetherValidator
 
 from contacts.models import Contact
 from domains.models import Domain
-from records.types import BaseRecord, Record
 from shorturls.models import ShortUrl
 from subdomains.models import Subdomain
 
@@ -64,91 +63,3 @@ class SubdomainSerializer(serializers.ModelSerializer):
 
     def get_full_name(self, obj):
         return str(obj)
-
-
-class BaseRecordSerializer(serializers.Serializer):
-    class TypeField(serializers.ChoiceField):
-        def __init__(self, **kwargs):
-            choices = sorted(map(lambda e: (e[0], f'{e[0]} - {e[1]}'), BaseRecord.get_available_types().items()))
-            super().__init__(choices, **kwargs)
-
-    name = serializers.CharField(max_length=255)
-    ttl = serializers.IntegerField(min_value=0, max_value=65535)
-    type = TypeField()
-    data = serializers.CharField()
-
-    def create(self, validated_data):
-        return BaseRecord(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.ttl = validated_data.get('ttl', instance.ttl)
-        instance.type = validated_data.get('type', instance.type)
-        instance.data = validated_data.get('data', instance.data)
-        return instance
-
-
-class RecordSerializer(BaseRecordSerializer):
-    class NameField(serializers.CharField):
-        def __init__(self, **kwargs):
-            kwargs['source'] = '*'
-            super().__init__(**kwargs)
-
-        def to_internal_value(self, data):
-            self.source_attrs = [self.field_name]
-            return super().to_internal_value(data)
-
-        def to_representation(self, value):
-            if type(value) is str:
-                if value[0] == '_':
-                    return Record.parse_name_srv(value)[2]
-                return value
-            return value.get_name()
-
-    name = NameField(max_length=255)
-    full_name = serializers.SerializerMethodField()
-    data = serializers.CharField(read_only=True)
-
-    id = serializers.IntegerField(read_only=True)
-
-    service = serializers.CharField(required=False)
-    protocol = serializers.CharField(required=False)
-
-    priority = serializers.IntegerField(min_value=0, max_value=65535, required=False)
-    weight = serializers.IntegerField(min_value=0, max_value=65535, required=False)
-    port = serializers.IntegerField(min_value=0, max_value=65535, required=False)
-
-    target = serializers.CharField()
-
-    def create(self, validated_data):
-        name = validated_data.get('name')
-        ttl = validated_data.get('ttl')
-        type = validated_data.get('type')
-        target = validated_data.get('target')
-        kwargs = {}
-        if type == 'MX' or type == 'SRV':
-            kwargs['priority'] = validated_data.get('priority')
-        if type == 'SRV':
-            kwargs['service'] = validated_data.get('service')
-            kwargs['protocol'] = validated_data.get('protocol')
-            kwargs['weight'] = validated_data.get('weight')
-            kwargs['port'] = validated_data.get('port')
-        instance = Record(name, ttl, type, target, **kwargs)
-        return instance
-
-    def update(self, instance, validated_data):
-        instance = super(RecordSerializer, self).update(instance, validated_data)
-        instance.id = validated_data.get('id', instance.id)
-        instance.target = validated_data.get('target', instance.target)
-        if instance.type == 'MX' or instance.type == 'SRV':
-            instance.priority = validated_data.get('priority', instance.priority)
-        if instance.type == 'SRV':
-            instance.service = validated_data.get('service', instance.service)
-            instance.protocol = validated_data.get('protocol', instance.protocol)
-            instance.name = f'{instance.service}.{instance.protocol}.{instance.name}'
-            instance.weight = validated_data.get('weight', instance.weight)
-            instance.port = validated_data.get('port', instance.port)
-        return instance
-
-    def get_full_name(self, obj):
-        return obj.name
