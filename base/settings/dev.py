0a1df3f4c8a6df77ff720d334c59125372855dfe
Seokjin Yoon <sjy5386@gmail.com>
2022-10-03 13:48:59 +0900

    Update dev settings

--- base/settings/dev.py
+++ base/settings/dev.py
@@ -2,9 +2,9 @@ from .common import *
 
 DEBUG = True
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
