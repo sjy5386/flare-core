6aadb8c00b2a48f4783f78bd707adab193793dc0
Seokjin Yoon <sjy5386@gmail.com>
2022-10-17 22:24:03 +0900

    Add ProfileUpdateForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm, UsernameField, UserChangeForm
 
 
 class RegisterForm(UserCreationForm):
@@ -12,3 +12,11 @@ class RegisterForm(UserCreationForm):
         model = get_user_model()
         fields = ("username",)
         field_classes = {"username": UsernameField}
+
+
+class ProfileUpdateForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'email',)
