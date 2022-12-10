ada5d9d8080fbdcdd0c24bddd14084404ee76a76
Seokjin Yoon <sjy5386@gmail.com>
2022-12-10 19:30:12 +0900

    Add SubdomainStatus admin

--- subdomains/admin.py
+++ subdomains/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Subdomain, ReservedName, SubdomainStatus
 
 admin.site.register(Subdomain)
 admin.site.register(ReservedName)
+admin.site.register(SubdomainStatus)
