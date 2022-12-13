c3bc17d2798226fb94b65ef220520851c186f103
Seokjin Yoon <sjy5386@gmail.com>
2022-12-13 22:26:32 +0900

    Add AuthenticationToken model

--- dynamic_dns/models.py
+++ dynamic_dns/models.py
@@ -1,3 +1,20 @@
+import datetime
+import secrets
+
 from django.db import models
 
-# Create your models here.
+from records.models import Record
+
+
+class AuthenticationToken(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+    token = models.CharField(primary_key=True, max_length=32)
+    expire_at = models.DateTimeField(null=True)
+
+    @classmethod
+    def create(cls, record: Record) -> 'AuthenticationToken':
+        return cls(token=secrets.token_hex(16), expire_at=datetime.datetime.now() + datetime.timedelta(days=90),
+                   record=record)
