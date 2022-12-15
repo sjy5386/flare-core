1ccd7fae0b3a4b561e8d90955840c74114483450
Seokjin Yoon <sjy5386@gmail.com>
2022-12-15 19:49:29 +0900

    Add AuthenticationTokenForm

---
+++ dynamic_dns/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import AuthenticationToken
+
+
+class AuthenticationTokenForm(forms.ModelForm):
+    class Meta:
+        model = AuthenticationToken
+        fields = ('record',)
