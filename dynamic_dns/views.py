9cd7d7f025ca3e1d3eb37ae0c19846ad7ca9dd9b
Seokjin Yoon <sjy5386@gmail.com>
2022-12-17 18:36:13 +0900

    Update dynamic_dns view

--- dynamic_dns/views.py
+++ dynamic_dns/views.py
@@ -1,3 +1,5 @@
+import ipaddress
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
@@ -16,19 +18,26 @@ from .models import AuthenticationToken
 def dynamic_dns(request: HttpRequest, token: str) -> HttpResponse:
     authentication_token = get_object_or_404(AuthenticationToken, token=token)
     if authentication_token.has_expired():
-        return HttpResponse(status=401)
+        return HttpResponse('This authentication token has expired.', status=401)
     record = authentication_token.record
     if record.type not in ('A', 'AAAA'):
-        return HttpResponse(status=400)
+        return HttpResponse('Record type is not A or AAAA.', status=400)
     if request.method == 'GET':
         return HttpResponse(record.target)
     elif request.method == 'POST':
-        ip_address = get_remote_ip_address(request)
-        if ip_address == record.target:
+        remote_ip_address = get_remote_ip_address(request)
+        try:
+            ip_address = ipaddress.ip_address(remote_ip_address)
+            if not ((type(ip_address) == ipaddress.IPv4Address) and (record.type == 'A')) or not (
+                    (type(ip_address) == ipaddress.IPv6Address) and (record.type == 'AAAA')):
+                return HttpResponse('IP address version and record type do not match.', status=400)
+        except ValueError:
+            return HttpResponse('Invalid IP address.', status=400)
+        if remote_ip_address == record.target:
             return HttpResponse(False)
         subdomain = get_object_or_404(Subdomain, name=record.subdomain_name, domain=record.domain)
-        Record.update_record(PROVIDER_CLASS(), subdomain, record.id, target=ip_address)
-        return HttpResponse(ip_address == record.target)
+        Record.update_record(PROVIDER_CLASS(), subdomain, record.id, target=remote_ip_address)
+        return HttpResponse(remote_ip_address == record.target)
     else:
         return HttpResponse(status=405)
 
