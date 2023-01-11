0865482b37fb42afcb52fc7e9127e17af5c04239
Seokjin Yoon <sjy5386@gmail.com>
2023-01-12 00:06:19 +0900

    Add whois api

--- apis/urls.py
+++ apis/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('whois/', views.whois, name='whois'),
 ]
