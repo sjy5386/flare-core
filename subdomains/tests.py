18ce1f78d0e5ec3904494b537d830c7d92cd8a89
Seokjin Yoon <sjy5386@gmail.com>
2023-01-15 09:31:32 +0900

    Add SubdomainTest

--- subdomains/tests.py
+++ subdomains/tests.py
@@ -1,3 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from subdomains.validators import validate_domain_name
+
+
+class SubdomainTest(TestCase):
+    def test_validate_subdomain_name(self):
+        success_values = (
+            'helloworld', 'hello-world', 'helloworld1',
+        )
+        failure_values = (
+            'helloworld-', 'Hello, world!', '안녕세상',
+        )
+        for value in success_values:
+            validate_domain_name(value)
+        with self.assertRaises(ValidationError):
+            for value in failure_values:
+                validate_domain_name(value)
