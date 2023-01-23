def5147f3ad4ad630877c52a129c55109ec8d6a9
Seokjin Yoon <sjy5386@gmail.com>
2023-01-23 17:08:07 +0900

    Change return value type

--- shorturls/providers/base.py
+++ shorturls/providers/base.py
@@ -1,6 +1,8 @@
+from typing import Dict, Any
+
 from domains.models import Domain
 
 
 class BaseShortUrlProvider:
-    def create_short_url(self, domain: Domain, long_url: str) -> str:
+    def create_short_url(self, domain: Domain, long_url: str) -> Dict[str, Any]:
         pass
