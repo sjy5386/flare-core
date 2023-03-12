8028fef67e08fbd3204c3adbade5cabe6799e7c8
Seokjin Yoon <sjy5386@gmail.com>
2023-03-12 18:01:08 +0900

    Add Parking model

--- page_rules/models.py
+++ page_rules/models.py
@@ -14,3 +14,13 @@ class Forwarding(models.Model):
     destination_url = models.URLField('Destination URL')
     http_status_code = models.IntegerField('HTTP Status Code', choices=HttpStatusCodeRedirection.choices)
     force_path_root = models.BooleanField('Force path root?')
+
+
+class Parking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    domain_name = models.CharField(max_length=255)
+
+    title = models.CharField('Title', max_length=63, blank=True)
+    content = models.TextField('Content', blank=True)
