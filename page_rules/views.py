89b52c8824eaa483fe75fe47490afa3288188851
Seokjin Yoon <sjy5386@gmail.com>
2023-03-12 18:29:24 +0900

    Add park view

--- page_rules/views.py
+++ page_rules/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_GET
 
-# Create your views here.
+from .models import Parking
+
+
+@require_GET
+def park(request: HttpRequest, parking: Parking = None) -> HttpResponse:
+    if parking is None:
+        parking = get_object_or_404(Parking, domain_name=request.META.get('HTTP_HOST'))
+    return render(request, 'page_rules/parking.html', model_to_dict(parking))
