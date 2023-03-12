65fd09580c719066e74e44caf360c6e6e42e56e1
Seokjin Yoon <sjy5386@gmail.com>
2023-03-12 17:56:38 +0900

    Add Forwarding model

--- page_rules/models.py
+++ page_rules/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Forwarding(models.Model):
+    class HttpStatusCodeRedirection(models.IntegerChoices):
+        MOVED_PERMANENTLY = 301
+        FOUND = 302
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    domain_name = models.CharField(max_length=255)
+
+    destination_url = models.URLField('Destination URL')
+    http_status_code = models.IntegerField('HTTP Status Code', choices=HttpStatusCodeRedirection.choices)
+    force_path_root = models.BooleanField('Force path root?')
