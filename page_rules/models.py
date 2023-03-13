6b58dc75d339314483102ef2854b67c474729f3d
Seokjin Yoon <sjy5386@gmail.com>
2023-03-13 21:05:19 +0900

    Rename Forwarding to WebForwarding

--- page_rules/models.py
+++ page_rules/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Forwarding(models.Model):
+class WebForwarding(models.Model):
     class HttpStatusCodeRedirection(models.IntegerChoices):
         MOVED_PERMANENTLY = 301
         FOUND = 302
--- page_rules/views.py
+++ page_rules/views.py
@@ -3,20 +3,20 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_GET
 
-from .models import Forwarding, Parking
+from .models import WebForwarding, Parking
 
 
 @require_GET
-def forward(request: HttpRequest, forwarding: Forwarding = None) -> HttpResponse:
-    if forwarding is None and False:
-        forwarding = get_object_or_404(Forwarding, domain_name=request.META.get('HTTP_HOST'))
-    destination_url = forwarding.destination_url
-    if not forwarding.force_path_root:
+def forward_web(request: HttpRequest, web_forwarding: WebForwarding = None) -> HttpResponse:
+    if web_forwarding is None and False:
+        web_forwarding = get_object_or_404(WebForwarding, domain_name=request.META.get('HTTP_HOST'))
+    destination_url = web_forwarding.destination_url
+    if not web_forwarding.force_path_root:
         if destination_url.endswith('/'):
             destination_url = destination_url[:-1]
         destination_url += request.META.get('PATH_INFO') + '?' + request.META.get('QUERY_STRING')
     return redirect(destination_url,
-                    permanent=forwarding.http_status_code == Forwarding.HttpStatusCodeRedirection.MOVED_PERMANENTLY)
+                    permanent=web_forwarding.http_status_code == WebForwarding.HttpStatusCodeRedirection.MOVED_PERMANENTLY)
 
 
 @require_GET
