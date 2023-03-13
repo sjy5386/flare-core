65d9334736325e9646248dbea3270b3b2d75ec29
Seokjin Yoon <sjy5386@gmail.com>
2023-03-13 21:37:03 +0900

    Add DomainParking admin

--- page_rules/admin.py
+++ page_rules/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import WebForwarding
+from .models import WebForwarding, DomainParking
 
 admin.site.register(WebForwarding)
+admin.site.register(DomainParking)
