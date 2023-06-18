af5621d2083bc27e13d625b7aa1355be321943ed
Seokjin Yoon <sjy5386@gmail.com>
2023-06-18 20:56:58 +0900

    Remove boards urls

--- base/urls.py
+++ base/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),
-    path('boards/', include('boards.urls')),
     path('contacts/', include('contacts.urls')),
     path('domains/', include('domains.urls')),
     path('dynamic-dns/', include('dynamic_dns.urls')),
