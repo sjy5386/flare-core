f3919068baeaab702bc479b0601452840fdc0aa5
Seokjin Yoon <sjy5386@gmail.com>
2023-07-01 19:53:13 +0900

    Add CordMiddleware

--- base/settings/common.py
+++ base/settings/common.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
