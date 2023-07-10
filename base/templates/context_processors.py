fb4f1f21647e9d5df9e5ddb0031d8688e04435f2
Seokjin Yoon <sjy5386@gmail.com>
2023-07-11 00:11:34 +0900

    Update site_name context processor

--- base/templates/context_processors.py
+++ base/templates/context_processors.py
@@ -6,4 +6,5 @@ from base.settings.common import SITE_NAME
 def site_name(request: HttpRequest) -> dict[str, str]:
     return {
         'site_name': SITE_NAME,
+        'site_domain_name': request.META.get('HTTP_HOST', 'subshorts.com'),
     }
