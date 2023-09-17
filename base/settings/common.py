9b1a014491295b6e25581c0910ac487bc13bcfb5
Seokjin Yoon <sjy5386@gmail.com>
2023-09-17 18:41:10 +0900

    Move static directory

--- base/settings/common.py
+++ base/settings/common.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'docker/nginx/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'out/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
