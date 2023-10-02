d8b8a3b5b987730cec996a2f41eced8a5bda140b
Seokjin Yoon <sjy5386@gmail.com>
2023-10-02 18:22:46 +0900

    Add require email pipeline

--- base/settings/common.py
+++ base/settings/common.py
@@ -244,6 +244,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
+    'accounts.pipelines.require_email',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.mail.mail_validation',
     'social_core.pipeline.social_auth.associate_by_email',
