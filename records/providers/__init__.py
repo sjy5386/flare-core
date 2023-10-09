5a76563aad74ccd2386d9d700b0f054139c12323
Seokjin Yoon <sjy5386@gmail.com>
2023-10-09 23:29:20 +0900

    Add RecordProvider enum

--- records/providers/__init__.py
+++ records/providers/__init__.py
@@ -1,8 +1,22 @@
+from enum import Enum
+
 from domains.models import Domain
 from .base import BaseRecordProvider
 from .digitalocean import DigitalOceanRecordProvider
+from .linode import LinodeRecordProvider
+from .vultr import VultrRecordProvider
 
-PROVIDER_CLASS = DigitalOceanRecordProvider
+
+class RecordProvider(Enum):
+    DIGITALOCEAN = (DigitalOceanRecordProvider,)
+    LINODE = (LinodeRecordProvider,)
+    VULTR = (VultrRecordProvider,)
+
+    def __init__(self, provider_class: type[BaseRecordProvider]):
+        self.provider_class = provider_class
+
+
+PROVIDER_CLASS = RecordProvider.DIGITALOCEAN.provider_class
 
 
 def get_record_provider(domain: Domain) -> BaseRecordProvider:
