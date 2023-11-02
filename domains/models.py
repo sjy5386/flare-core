f7c2472d6e7d7ae90ddb8203ccfec9fd57086503
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2023-11-02 21:43:46 +0900

    Add user field
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- domains/models.py
+++ domains/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from base.settings.common import AUTH_USER_MODEL
+
 
 class Domain(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -7,6 +9,8 @@ class Domain(models.Model):
 
     name = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
+
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.RESTRICT)  # Domain registrant or administrator
 
     def __str__(self):
         return self.name
