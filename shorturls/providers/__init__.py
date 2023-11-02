933f6ac4a7953f1a450f90b6e5869f1f72766145
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2023-11-02 22:49:38 +0900

    Remove DEFAULT_SHORT_URL_PROVIDER environment variable
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- shorturls/providers/__init__.py
+++ shorturls/providers/__init__.py
@@ -1,4 +1,3 @@
-import os
 from enum import Enum
 
 from domains.models import Domain
@@ -13,9 +12,6 @@ class ShortUrlProvider(Enum):
 
     def __init__(self, provider_class: type[BaseShortUrlProvider]):
         self.provider_class = provider_class
-
-
-DEFAULT_SHORT_URL_PROVIDER = os.environ.get('DEFAULT_SHORT_URL_PROVIDER') or ShortUrlProvider.FIREBASE_DYNAMIC_LINKS.name
 
 
 def get_short_url_provider(domain: Domain | None) -> BaseShortUrlProvider | None:
