0dec6674da6880d2d1ac5efa0ba2b0f4138a716b
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2023-11-10 21:45:24 +0900

    Update CloudflareDnsRecordProvider
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- records/providers/cloudflare.py
+++ records/providers/cloudflare.py
@@ -1,14 +1,27 @@
 from typing import Any
 
+import requests
+
 from domains.models import Domain
 from .base import BaseDnsRecordProvider
+from ..exceptions import RecordProviderError
+from ..models import Record
 
 
 class CloudflareDnsRecordProvider(BaseDnsRecordProvider):
     host = 'https://api.cloudflare.com'
 
     def list_records(self, subdomain_name: str, domain: Domain) -> list[dict[str, Any]]:
-        pass
+        response = requests.get(self.host + f'/client/v4/zones/{self.get_zone_identifier(domain.name)}/dns_records',
+                                params={
+                                    'per_page': 50000,
+                                })
+        try:
+            response.raise_for_status()
+        except requests.HTTPError:
+            raise RecordProviderError(response.json())
+        return list(filter(lambda x: x.get('name').endswith(subdomain_name),
+                           map(self.from_cloudflare_record, response.json().get('result'))))
 
     def create_record(self, subdomain_name: str, domain: Domain, **kwargs) -> dict[str, Any]:
         pass
@@ -24,3 +37,30 @@ class CloudflareDnsRecordProvider(BaseDnsRecordProvider):
 
     def get_nameservers(self, domain: Domain = None) -> list[str]:
         pass
+
+    def get_zone_identifier(self, domain_name: str) -> str:
+        response = requests.get(self.host + '/client/v4/zones')
+        try:
+            response.raise_for_status()
+        except requests.HTTPError:
+            raise RecordProviderError(response.json())
+        zone_identifier = next(map(lambda x: x.get('id'),
+                                   filter(lambda x: x.get('name') == domain_name, response.json().get('result', []))))
+        return zone_identifier
+
+    @staticmethod
+    def from_cloudflare_record(cloudflare_record: dict[str, Any]) -> dict[str, Any]:
+        service, protocol, name = Record.split_name(cloudflare_record.get('name'))
+        priority, weight, port, target = Record.split_data(cloudflare_record.get('content'))
+        return {
+            'provider_id': cloudflare_record.get('id'),
+            'name': name,
+            'ttl': cloudflare_record.get('ttl'),
+            'type': cloudflare_record.get('type'),
+            'service': service,
+            'protocol': protocol,
+            'target': target,
+            'priority': priority,
+            'weight': weight,
+            'port': port,
+        }
