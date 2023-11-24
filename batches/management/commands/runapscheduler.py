b10d8aba4398675e69f319b75e92cf906838aaee
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2023-11-25 00:00:38 +0900

    Log if APPLICATION_TYPE is not BATCH
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- batches/management/commands/runapscheduler.py
+++ batches/management/commands/runapscheduler.py
@@ -2,6 +2,8 @@ import logging
 
 from django.core.management import BaseCommand
 
+from base.settings import APPLICATION_TYPE, ApplicationType
+
 
 class Command(BaseCommand):
     help = 'Runs APScheduler.'
@@ -10,3 +12,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.info('========== Starting batch application ==========')
+        if APPLICATION_TYPE != ApplicationType.BATCH:
+            self.logger.warning('APPLICATION_TYPE is not BATCH.')
