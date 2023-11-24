71971360a4e96b7615c9f589da2fefda439955df
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2023-11-25 00:27:11 +0900

    Update runapscheduler command
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- batches/management/commands/runapscheduler.py
+++ batches/management/commands/runapscheduler.py
@@ -1,6 +1,8 @@
 import logging
 
+from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
 
 from base.settings import APPLICATION_TYPE, ApplicationType
 
@@ -14,3 +16,11 @@ class Command(BaseCommand):
         self.logger.info('========== Starting batch application ==========')
         if APPLICATION_TYPE != ApplicationType.BATCH:
             self.logger.warning('APPLICATION_TYPE is not BATCH.')
+        scheduler = BlockingScheduler()
+        scheduler.add_jobstore(DjangoJobStore(), 'default')
+        try:
+            scheduler.start()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
+        finally:
+            self.logger.info('========== Ending batch application ==========')
