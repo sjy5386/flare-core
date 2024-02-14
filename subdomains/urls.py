4c8f48ef0389b49947d8391a98780699f06a7122
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-02-15 00:11:48 +0900

    Update subdomain views
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- subdomains/urls.py
+++ subdomains/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('whois/', views.WhoisView.as_view(), name='whois'),
     path('contact/', views.SubdomainContactView.as_view(), name='contact'),
     path('create/', views.SubdomainCreateView.as_view(), name='create'),
-    path('<int:id>/', views.SubdomainDetailView.as_view(), name='detail'),
-    path('<int:id>/update/', views.SubdomainUpdateView.as_view(), name='update'),
-    path('<int:id>/delete/', views.SubdomainDeleteView.as_view(), name='delete'),
+    path('<str:id>/', views.SubdomainDetailView.as_view(), name='detail'),
+    path('<str:id>/update/', views.SubdomainUpdateView.as_view(), name='update'),
+    path('<str:id>/delete/', views.SubdomainDeleteView.as_view(), name='delete'),
 ]
