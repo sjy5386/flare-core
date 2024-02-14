f297c917fbdd10ff87dd674fc301e9c9119f5a24
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-02-15 00:04:56 +0900

    Update short url views
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- shorturls/urls.py
+++ shorturls/urls.py
@@ -6,5 +6,5 @@ app_name = 'short_urls'
 urlpatterns = [
     path('', views.ShortUrlListView.as_view(), name='list'),
     path('create/', views.ShortUrlCreateView.as_view(), name='create'),
-    path('<int:id>/', views.ShortUrlDetailView.as_view(), name='detail'),
+    path('<str:id>/', views.ShortUrlDetailView.as_view(), name='detail'),
 ]
