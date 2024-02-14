1b6728b5fa96e5dce07054483fec7f0382a11b4b
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-02-14 22:30:37 +0900

    Update DomainSerializer
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- domains/serializers.py
+++ domains/serializers.py
@@ -6,4 +6,14 @@ from .models import Domain
 class DomainSerializer(serializers.ModelSerializer):
     class Meta:
         model = Domain
-        fields = ['id', 'name']
+        fields = (
+            'uuid',
+            'created_at',
+            'updated_at',
+            'name',
+        )
+        read_only_fields = (
+            'uuid',
+            'created_at',
+            'updated_at',
+        )
