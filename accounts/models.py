3a097d80c30158a2a02a2ecd074a6c067d3f37f6
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-02-14 21:54:08 +0900

    Add User.uuid field
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    uuid = models.UUIDField(primary_key=False, unique=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
