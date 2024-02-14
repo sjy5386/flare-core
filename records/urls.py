3e6c5f6f26d01f88f4abab0517042831a64c40bc
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-02-15 00:19:34 +0900

    Update dns record views
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- records/urls.py
+++ records/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('export/', views.ZoneExportView.as_view(), name='zone_export'),
     path('import/', views.ZoneImportView.as_view(), name='zone_import'),
     path('create/', views.DnsRecordCreateView.as_view(), name='create'),
-    path('<int:id>/', views.DnsRecordDetailView.as_view(), name='detail'),
-    path('<int:id>/update/', views.DnsRecordUpdateView.as_view(), name='update'),
-    path('<int:id>/delete/', views.DnsRecordDeleteView.as_view(), name='delete'),
+    path('<str:id>/', views.DnsRecordDetailView.as_view(), name='detail'),
+    path('<str:id>/update/', views.DnsRecordUpdateView.as_view(), name='update'),
+    path('<str:id>/delete/', views.DnsRecordDeleteView.as_view(), name='delete'),
 ]
