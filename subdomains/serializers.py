b17e7211605943f7e4988a050e330b6ffbc3b38b
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-02-15 00:39:29 +0900

    Update SubdomainSerializer
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- subdomains/serializers.py
+++ subdomains/serializers.py
@@ -46,15 +46,20 @@ class SubdomainSerializer(serializers.ModelSerializer):
                 fields=('name', 'domain',),
             ),
         ]
+        extra_kwargs = {
+            'domain': {
+                'write_only': True,
+            },
+        }
 
     def get_fields(self):
         fields = super(SubdomainSerializer, self).get_fields()
         request = self.context.get('request')
         contacts = Contact.objects.filter(user=request.user)
         fields.update({
-            'registrant': serializers.PrimaryKeyRelatedField(queryset=contacts),
-            'admin': serializers.PrimaryKeyRelatedField(queryset=contacts),
-            'tech': serializers.PrimaryKeyRelatedField(queryset=contacts),
-            'billing': serializers.PrimaryKeyRelatedField(queryset=contacts),
+            'registrant': serializers.PrimaryKeyRelatedField(queryset=contacts, write_only=True),
+            'admin': serializers.PrimaryKeyRelatedField(queryset=contacts, write_only=True),
+            'tech': serializers.PrimaryKeyRelatedField(queryset=contacts, write_only=True),
+            'billing': serializers.PrimaryKeyRelatedField(queryset=contacts, write_only=True),
         })
         return fields
