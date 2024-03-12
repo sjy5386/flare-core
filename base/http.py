59f67eeee28acd1172fa5985d5875e770721728f
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-03-12 21:13:52 +0900

    Add is_private_ip_address function
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- base/http.py
+++ base/http.py
@@ -1,3 +1,5 @@
+import ipaddress
+
 from django.http import HttpRequest
 
 
@@ -5,3 +7,11 @@ def get_remote_ip_address(request: HttpRequest) -> str:
     return list(map(lambda x: x.strip(),
                     request.META.get('HTTP_X_FORWARDED_FOR',
                                      request.META.get('REMOTE_ADDR')).split(',')))[0]
+
+
+def is_private_ip_address(ip_address: str) -> bool:
+    return any(ipaddress.ip_address(ip_address) in network for network in map(ipaddress.ip_network, (
+        '10.0.0.0/8',
+        '172.16.0.0/12',
+        '192.168.0.0/16',
+    )))
