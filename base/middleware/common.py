b17dfe1b6c990bf22d36f4375e70fc2ca4f746f4
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2024-09-26 00:38:26 +0900

    Update Debug ID
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- base/middleware/common.py
+++ base/middleware/common.py
@@ -9,8 +9,8 @@ from .base import BaseMiddleware
 class CommonMiddleware(BaseMiddleware):
     def __call__(self, request: HttpRequest) -> HttpResponse:
         debug_id = uuid.uuid4().hex
-        self.logger.info(f'Debug ID: {debug_id}')
         threading.current_thread().name = debug_id
+        self.logger.info(f'Debug ID: {debug_id}')
         response: HttpResponse = self.get_response(request)
         response.headers['Flare-Debug-Id'] = debug_id
         return response
